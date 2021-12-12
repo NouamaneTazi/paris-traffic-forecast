6df05f2661582306bf7e0517f45927852bc1d69e
Nouamane Tazi <nouamane98@gmail.com>
2021-12-12 05:18:44 +0100

    train TFT model on sagemaker

--- src/train_TFT.py
+++ src/train_TFT.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
+import joblib
+
+# from matplotlib import pyplot as plt
 
 import torch
 
-from darts import TimeSeries
-from darts.dataprocessing.transformers import Scaler
 from darts.models import TFTModel
-from darts.utils.timeseries_generation import datetime_attribute_timeseries
 import warnings
+
+from utils import preprocess_data
 
 warnings.filterwarnings("ignore")
 
@@ -20,6 +22,13 @@ import argparse
 import os
 import json
 
+# # set environment for sagemaker fir local testing
+# os.environ["SM_HOSTS"] = '["to-be-filled-by-sagemaker"]'
+# os.environ["SM_CURRENT_HOST"] = "to-be-filled-by-sagemaker"
+# os.environ["SM_MODEL_DIR"] = "models"
+# os.environ["SM_CHANNEL_TRAINING"] = "tmp"
+
+
 # sagemaker
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,64 +38,28 @@ if __name__ == "__main__":
     parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--data_dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
 
-    parser.add_argument("--train_fileName", type=str, default="convention-2014-2021.csv-proc.csv")
-    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--train_fileName", type=str, default="voi-convention2021.pkl")
+    parser.add_argument("--pretrain_epochs", type=int, default=1)
+    parser.add_argument("--finetune_epochs", type=int, default=1)
 
     args = parser.parse_args()
 
-    # ## load data
-    # df = pd.read_csv("processed_data/convention-2014-2021.csv-proc.csv").set_index('time')
-    df = pd.read_csv(args.data_dir + "/" + args.train_fileName).set_index("time")
-    df.index = pd.to_datetime(df.index, utc=False)
-    df.sort_index(inplace=True)
-    df
+    target_noeuds = ["Lecourbe-Convention", "Convention-Blomet"]
 
-    value_cols = ["debit", "occupation"]
-    cov_cols = ["etat_barre"]
-    ts = TimeSeries.from_dataframe(df, value_cols=value_cols, fill_missing_dates=True, freq="H")
-    past_covs = TimeSeries.from_dataframe(df, value_cols=cov_cols, fill_missing_dates=True, freq="H")
-    series = ts
+    # ## load data
+    all_arcs = joblib.load(f"{args.data_dir}/{args.train_fileName}")  # voi-convention1.pkl
+    print(f"Loaded {len(all_arcs)} arcs:")
+    for arc in all_arcs:
+        print(f"{arc['noeud_amont']} {arc['noeud_aval']}")
+
+    all_arcs = preprocess_data(all_arcs)
+
+    for arc in all_arcs:
+        # set final target arc
+        if [arc["noeud_amont"], arc["noeud_aval"]] == target_noeuds:
+            target_arc = arc
 
     # ## model
-
-    # ts.drop_before(pd.Timestamp("2021-11-20")).univariate_component(0).plot(label='Débit horaire', new_plot=True)
-
-    # Create training and validation sets:
-    # training_cutoff = pd.Timestamp('2021-12-01')
-    # train, val = ts.split_after(training_cutoff)
-    train = ts
-    past_covs_train = past_covs
-
-    # Normalize the time series (note: we avoid fitting the transformer on the validation set)
-    transformer = Scaler()
-    train_transformed = transformer.fit_transform(train)
-    # val_transformed = transformer.transform(val)
-    series_transformed = transformer.transform(ts)
-
-    # create year, month and integer index covariate series
-    covariates = datetime_attribute_timeseries(series, attribute="year", one_hot=False)
-    covariates = covariates.stack(datetime_attribute_timeseries(series, attribute="month", one_hot=False))
-    covariates = covariates.stack(datetime_attribute_timeseries(series, attribute="day", one_hot=False))
-    covariates = covariates.stack(datetime_attribute_timeseries(series, attribute="hour", one_hot=False))
-    covariates = covariates.stack(
-        TimeSeries.from_times_and_values(
-            times=series.time_index, values=np.arange(len(series)), columns=["linear_increase"]
-        )
-    )
-    covariates
-
-    # transform past covs
-    past_scaler_covs = Scaler()
-    past_scaler_covs.fit(past_covs_train)
-    past_covariates_transformed = past_scaler_covs.transform(past_covs_train)
-
-    # transform covariates (note: we fit the transformer on train split and can then transform the entire covariates series)
-    scaler_covs = Scaler()
-    cov_train = covariates
-    scaler_covs.fit(cov_train)
-    covariates_transformed = scaler_covs.transform(covariates)
-
-    # default quantiles for QuantileRegression
 
     input_chunk_length = 168  # week
     forecast_horizon = 24  # day
@@ -98,7 +71,6 @@ if __name__ == "__main__":
         num_attention_heads=4,
         dropout=0.3,
         batch_size=128,
-        n_epochs=args.epochs,
         add_relative_index=False,
         add_cyclic_encoder="day",
         likelihood=None,  # QuantileRegression is set per default
@@ -106,20 +78,49 @@ if __name__ == "__main__":
         random_state=42,
     )
 
-    print("Start fitting model")
-    my_model.fit(
-        train_transformed,
-        past_covariates=past_covariates_transformed,
-        future_covariates=covariates_transformed,
-        verbose=True,
-    )
+    try:
+        print("Fitting model on all data...")
+        my_model.fit(
+            [arc["train_scaled"] for arc in all_arcs],
+            past_covariates=[arc["past_covs_scaled"].univariate_component(0) for arc in all_arcs],
+            future_covariates=[arc["past_covs_scaled"].univariate_component(1) for arc in all_arcs],
+            verbose=True,
+            epochs=args.pretrain_epochs,
+        )
 
-    print(f"Saving model to {args.model_dir}")
-    torch.save(my_model, args.model_dir + "/model.pth")
+        print("Finetuning model on target data...")
+        my_model.fit(
+            target_arc["train_scaled"],
+            past_covariates=target_arc["past_covs_scaled"].univariate_component(0),
+            future_covariates=target_arc["past_covs_scaled"].univariate_component(1),
+            verbose=True,
+            epochs=args.finetune_epochs,
+        )
 
-    past_covariates_transformed = past_covariates_transformed.append_values([1] * 24 * 10)
-    pred_ts = my_model.predict(n=24 * 5, num_samples=1, past_covariates=past_covariates_transformed)
-    pred_ts = transformer.inverse_transform(pred_ts)
+        print(f"Saving model to {args.model_dir}")
+        torch.save(my_model, args.model_dir + "/TFT-model.pth")
 
-    pred_ts.to_csv(args.model_dir + "preds.csv")
-    print("Preds saved successfully!")
+        forecast_horizon = 24 * 5  # predict 5 days
+        past_covs = target_arc["past_covs_scaled"].univariate_component(0).append_values(np.ones(forecast_horizon))
+        future_covs = target_arc["past_covs_scaled"].univariate_component(1).append_values(np.zeros(forecast_horizon))
+
+        # NOTE: fitting on multiple series gives the "need past_covariates" error
+        # to avoid that, we fit on a single series one last time and then predict using only future_covariates
+        pred_ts = my_model.predict(
+            forecast_horizon,
+            series=target_arc["train_scaled"],
+            past_covariates=past_covs,
+            future_covariates=future_covs,
+        )
+        # pred_ts.univariate_component(0).plot()
+        # target_arc["val_scaled"].univariate_component(0).plot()
+        # plt.show()
+
+        # pred_ts = target_arc["scaler"].inverse_transform(pred_ts)
+        pred_ts.to_csv(args.model_dir + "/preds.csv")
+        print("Preds saved successfully!")
+
+    except:
+        print("An error happened")
+        print(f"Saving model to {args.model_dir}")
+        torch.save(my_model, args.model_dir + "/TFT-model.pth")
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .plot import plot_after
-from .load import load_data
